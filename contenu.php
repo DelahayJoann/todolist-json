deefab86f698c2fc4e409a8c0934c09d321afa84
Delahay Joann <delahay.joann@gmail.com>
2020-11-18 14:50:33 +0100

    indent correction

--- contenu.php
+++ contenu.php
@@ -96,41 +96,41 @@
         $myToDo->save('todolist.serial');  
     }
 
-if(isset($_POST['checkedId'])){
-    $tmpArray = $myToDo->getToDoElements();
-    $tmpArray[$_POST['checkedId']]->setChecked();
-    $myToDo->save('todolist.serial');
-    $_POST = array();
-}
-if(isset($_POST['task'])){
-    foreach ($_POST['task'] as $key => $value){
-        $myToDo->sendArchive($value);
-    }
-    $myToDo->save('todolist.serial');
-    $_POST = array();
-}
-if(isset($_POST['addTask'])){
-    if(!empty($_POST['addTask'])){
-        $string = trim($_POST['addTask']);
-        $string = strip_tags($_POST['addTask']);
-        $string = filter_var($string, FILTER_SANITIZE_MAGIC_QUOTES);
-        $string = filter_var($string, FILTER_SANITIZE_STRING);
-        $taskElem = new ToDoElement($string);
-        $myToDo->addToDoElement($taskElem);
+    if(isset($_POST['checkedId'])){
+        $tmpArray = $myToDo->getToDoElements();
+        $tmpArray[$_POST['checkedId']]->setChecked();
         $myToDo->save('todolist.serial');
         $_POST = array();
     }
-}
-if(isset($_POST['swapId1'],$_POST['swapId2'])){
-    $myToDoArray = $myToDo->getToDoElements();
+    if(isset($_POST['task'])){
+        foreach ($_POST['task'] as $key => $value){
+            $myToDo->sendArchive($value);
+        }
+        $myToDo->save('todolist.serial');
+        $_POST = array();
+    }
+    if(isset($_POST['addTask'])){
+        if(!empty($_POST['addTask'])){
+            $string = trim($_POST['addTask']);
+            $string = strip_tags($_POST['addTask']);
+            $string = filter_var($string, FILTER_SANITIZE_MAGIC_QUOTES);
+            $string = filter_var($string, FILTER_SANITIZE_STRING);
+            $taskElem = new ToDoElement($string);
+            $myToDo->addToDoElement($taskElem);
+            $myToDo->save('todolist.serial');
+            $_POST = array();
+        }
+    }
+    if(isset($_POST['swapId1'],$_POST['swapId2'])){
+        $myToDoArray = $myToDo->getToDoElements();
 
-    $tmp = $myToDoArray[$_POST['swapId1']];
+        $tmp = $myToDoArray[$_POST['swapId1']];
 
-    $myToDoArray[intval($_POST['swapId1'])] = $myToDoArray[intval($_POST['swapId2'])];
-    $myToDoArray[intval($_POST['swapId2'])] = $tmp;
+        $myToDoArray[intval($_POST['swapId1'])] = $myToDoArray[intval($_POST['swapId2'])];
+        $myToDoArray[intval($_POST['swapId2'])] = $tmp;
 
-    $myToDo->setToDoElements($myToDoArray);
-    $myToDo->save('todolist.serial');
-    $_POST = array();
-}
+        $myToDo->setToDoElements($myToDoArray);
+        $myToDo->save('todolist.serial');
+        $_POST = array();
+    }
 ?>
